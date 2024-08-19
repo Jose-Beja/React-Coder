812df5f1db998a807091c14eb5fcbb0618b4c76a
Jose Beja <beja.jose@gmail.com>
2024-08-19 09:04:27 -0300

    sumamos algunas correcciones.

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -10,6 +10,7 @@ const NavBar = () => {
                     <form  className="container-fluid justify-content-start">
                         <button  className="btn btn-sm btn-outline-secondary" type="button">Indumentaria</button>
                         <button  className="btn btn-sm btn-outline-secondary" type="button">Equipamiento</button>
+                        <button  className="btn btn-sm btn-outline-secondary" type="button">Varios</button>
                         <button  className="btn btn-sm btn-outline-secondary" type="button">Quienes somos</button>
                     </form>
                     <div className="carro">
